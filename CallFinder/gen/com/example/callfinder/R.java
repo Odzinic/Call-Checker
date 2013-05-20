e31b5c0a511945735493fee9f2ae31dc6ef64c64
Omar Dzinic <Odzinic@gmail.com>
2013-05-20 18:31:34 -0400

    Removed button.xml

--- CallFinder/gen/com/example/callfinder/R.java
+++ CallFinder/gen/com/example/callfinder/R.java
@@ -24,13 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int button1=0x7f080001;
+        public static final int action_settings=0x7f080001;
         public static final int ll1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_find_calls=0x7f030000;
-        public static final int button=0x7f030001;
     }
     public static final class menu {
         public static final int find_calls=0x7f070000;
