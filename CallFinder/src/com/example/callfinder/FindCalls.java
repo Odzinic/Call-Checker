25f04fa28ce703e69dbbfecff91b333a30261ff6
Odzinic <Ozinic@gmail.com>
2013-05-20 19:36:59 -0400

    Buttons now contain numbers

--- CallFinder/src/com/example/callfinder/FindCalls.java
+++ CallFinder/src/com/example/callfinder/FindCalls.java
@@ -1,7 +1,12 @@
 package com.example.callfinder;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CallLog;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
@@ -11,23 +16,54 @@ public class FindCalls extends Activity {
 	Button b;
 	LinearLayout layout;
 	LayoutParams lp;
+	Cursor c;
+	Uri allCalls = Uri.parse("content://call_log/calls");
+	String num, name;
+	int type;
+	ArrayList arrlst = new ArrayList<Integer>();
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_find_calls);
-		initialize();
-		
+		checkLogs();
+
 	}
 
-	public void initialize() {
+	public void initialize(String Number) {
 		layout = (LinearLayout) findViewById(R.id.ll1);
 		lp = new LayoutParams(LayoutParams.MATCH_PARENT,
 				ViewGroup.LayoutParams.WRAP_CONTENT);
+
 		
-		for (int i=0; i<10; i++){
-		b = new Button(this);
-	
-		layout.addView(b, lp);
-		}
-		}
+			b = new Button(this);
+			b.setText(Number);
+
+			layout.addView(b, lp);
+		
 	}
+	
+	private void checkLogs() {
+		int i = 0;
+		
+		c = getContentResolver().query(allCalls, null, null, null, null);
+
+		while (c.moveToNext()) {
+			num = c.getString(c.getColumnIndex(CallLog.Calls.NUMBER));// for number
+			name = c.getString(c.getColumnIndex(CallLog.Calls.CACHED_NAME));// for  name
+			type = c.getInt(CallLog.Calls.INCOMING_TYPE);// for type
+			i++;
+			
+			if(type==0 && arrlst.contains(num) == false && name == null ){
+				arrlst.add(num);	
+				initialize(num);
+			}
+			
+			else{
+				arrlst.add(num);
+				continue;
+			}
+		}
+
+		c.close();
+	}
+}
